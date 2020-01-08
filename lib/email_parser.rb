b56c2f9d60e245a4470deb004cf667ba841064f5
Jin Wu <jinn.wuu@gmail.com>
2020-01-08 02:01:11 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,11 +1,11 @@
 class EmailAddressParser
+  attr_accessor :email_address
   
- def initialize(email_address)
-   @email_address = email_address
- end
- 
- def parse 
-   @email_address.delete(",").split.uniq
- end
- 
+  def initialize(email_address)
+    @email_address = email_address
+  end
+  
+  def parse
+    @email_address.delete(",").split.uniq
+  end
 end
